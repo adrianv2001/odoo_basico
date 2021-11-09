18ce01903072353deb98b57fcad1e3bbd43b98d5
a20adrianvp <adrian>
2021-11-09 10:41:02 +0100

    Basico escrito

--- models/informacion.py
+++ models/informacion.py
@@ -1,3 +1,14 @@
+# -*- coding: utf-8 -*-
+
 from odoo import models, fields, api
 
-autorizado = fields.Boolean(string="¿Autorizado?", default=True)
+
+class informacion(models.Model):
+     _name = 'odoo_basico.informacion'
+     _description = 'Módulo 2 de Odoo, Agrupamientos y Filtros'
+
+     autorizado = fields.Boolean(string="¿Autorizado?", default=True)
+     name = fields.Char(string='Titulo')
+     descripcion = fields.Text(string='Descripcion')
+     alto = fields.Integer(string='Alto en cm')
+
